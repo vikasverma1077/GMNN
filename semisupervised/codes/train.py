e2ccba9871a03efb40093247f1064b38060750fe
Vikas Verma <vermavik@blg5809.int.ets1.calculquebec.ca>
2019-08-29 11:56:36 -0400

    fix

--- semisupervised/codes/train.py
+++ semisupervised/codes/train.py
@@ -233,7 +233,7 @@ def pre_train(epoches):
         loss_aux = loss
         #loss, loss_aux = trainer_q.update_soft_aux(inputs_q, target_q, idx_train, epoch, opt)## for auxiliary net with shared parameters
         #import pdb; pdb.set_trace()
-        trainer_q.model.adj = adj
+        #trainer_q.model.adj = adj
         trainer_q.model.m1.adj = adj
         trainer_q.model.m2.adj = adj
         _, preds, accuracy_train = trainer_q.evaluate(inputs_q, target, idx_train) ## target_new : for augmented nodes
