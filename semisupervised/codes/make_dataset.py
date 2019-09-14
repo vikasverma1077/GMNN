879dd903ab49d0cc712d20f0f33b50087bc31297
Vikas Verma <vermavik@beluga3.int.ets1.calculquebec.ca>
2019-09-14 09:42:15 -0400

    data

--- semisupervised/codes/make_dataset.py
+++ semisupervised/codes/make_dataset.py
@@ -1,34 +1,44 @@
 import numpy as np
 from operator import __or__
+import os
+from distutils.dir_util import copy_tree
 
-data = np.loadtxt('../data/cora/label.txt').astype(int)
+
+data = np.loadtxt('../data/pubmed/label.txt').astype(int)
 labels = data[:,1]
 n_classes = len(np.unique(labels))
 
 
-n = 10
-n_valid = 10
-n_test = 100
 
+n = 20
+n_valid = 167
+#n_test = 100
 
-for i in np.arange(n_classes):
-    idx= data[labels==i,0]
-    np.random.shuffle(idx)
+for j in range(5):
+    new_data_dir = os.path.join('../data_subset/pubmed/n20', str(j+1))
+    os.makedirs(new_data_dir)
+    copy_tree('../data/pubmed', new_data_dir)
+
+    np.random.seed(j)
+
+    for i in np.arange(n_classes):
+        idx= data[labels==i,0]
+        np.random.shuffle(idx)
     
-    if i==0:
-        indices_train = idx[:n]
-        indices_valid = idx[n:n+n_valid]
-        indices_test = idx[n+n_valid:n+n_valid+n_test]
-    else:
-        indices_train = np.hstack((indices_train,idx[:n]))
-        indices_valid = np.hstack((indices_valid,idx[n:n+n_valid]))
-        indices_test = np.hstack((indices_test,idx[n+n_valid:n+n_valid+n_test]))
+        if i==0:
+            indices_train = idx[:n]
+            indices_valid = idx[n:n+n_valid]
+            indices_test = idx[n+n_valid:]
+        else:
+            indices_train = np.hstack((indices_train,idx[:n]))
+            indices_valid = np.hstack((indices_valid,idx[n:n+n_valid]))
+            indices_test = np.hstack((indices_test,idx[n+n_valid:]))
     
 
-np.random.shuffle(indices_train)
-np.random.shuffle(indices_valid)
-np.random.shuffle(indices_test)
+    np.random.shuffle(indices_train)
+    np.random.shuffle(indices_valid)
+    np.random.shuffle(indices_test)
 
-np.savetxt('../data/cora/train_temp.txt', indices_train, fmt='%d')
-np.savetxt('../data/cora/dev_temp.txt', indices_valid, fmt='%d')
-np.savetxt('../data/cora/test_temp.txt', indices_test, fmt='%d')
+    np.savetxt(os.path.join(new_data_dir,'train_temp.txt'), indices_train, fmt='%d')
+    np.savetxt(os.path.join(new_data_dir,'dev_temp.txt'), indices_valid, fmt='%d')
+    np.savetxt(os.path.join(new_data_dir,'test_temp.txt'), indices_test, fmt='%d')
