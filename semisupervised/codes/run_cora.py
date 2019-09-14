4adeb04d18a7b0fcbce5f8ebdc22b5267b3aba82
vikasverma1077 <vikasverma.iitm@gmail.com>
2019-09-14 07:12:59 -0400

    data

--- semisupervised/codes/run_cora.py
+++ semisupervised/codes/run_cora.py
@@ -43,7 +43,7 @@ def run(opt):
     opt_ = copy.deepcopy(opt)
     os.system(generate_command(opt_))
 
-"""
+
 for k in range(5):
     seed = k + 1
     opt['seed'] = seed
@@ -53,52 +53,3 @@ for k in range(5):
 
     run(opt)
     
-"""
-
-### create subset data###
-import numpy as np
-from operator import __or__
-from functools import reduce
-
-data = np.loadtxt('../data/cora/label.txt').astype(int)
-labels = data[:,1]
-n_classes = len(np.unique(labels))
-
-
-n = 15
-n_valid = 15
-n_test = 100
-print (n, n_valid, n_test)
-
-for i in np.arange(n_classes):
-    idx= data[labels==i,0]
-    np.random.shuffle(idx)
-                
-    if i==0:
-        indices_train = idx[:n]
-        indices_valid = idx[n:n+n_valid]
-        indices_test = idx[n+n_valid:n+n_valid+n_test]
-    else:
-        indices_train = np.hstack((indices_train,idx[:n]))
-        indices_valid = np.hstack((indices_valid,idx[n:n+n_valid]))
-        indices_test = np.hstack((indices_test,idx[n+n_valid:n+n_valid+n_test]))
-                                                                                    
-                                                                                                                    
-np.random.shuffle(indices_train)
-np.random.shuffle(indices_valid)
-np.random.shuffle(indices_test)
-np.savetxt('../data/cora/train_temp.txt', indices_train, fmt='%d')
-np.savetxt('../data/cora/dev_temp.txt', indices_valid, fmt='%d')   
-np.savetxt('../data/cora/test_temp.txt', indices_test, fmt='%d')
-
-### data creation ended ####
-
-for i in [0.1, 1.0, 2.0]:
-    for j in [1.0, 10.0, 20.0]:
-        opt['mixup_alpha'] = i
-        opt['mixup_consistency'] = j
-        print('mixup_alpha_'+str(opt['mixup_alpha']))
-        print('mixup_consistency_'+ str(opt['mixup_consistency']))
-
-        run(opt)
-
--- semisupervised/codes/train.py
+++ semisupervised/codes/train.py
@@ -392,14 +392,63 @@ def run(seed):
 
 
 
-acc_list =[]
-for i in np.arange(5):
-    acc_list.append(run(seed=i))
-    
-acc = np.asarray(acc_list)
-acc_mean = acc.mean()
-acc_std = acc.std()
-print ("mix_alpha, consis_coeff_"+ str(args.mixup_alpha)+'_'+ str(args.mixup_consistency)+':'+str(acc_mean)+'_'+str(acc_std))
+
+### create subset data###
+import numpy as np
+from operator import __or__
+from functools import reduce
+
+data = np.loadtxt('../data/cora/label.txt').astype(int)
+labels = data[:,1]
+n_classes = len(np.unique(labels))
+
+
+n = 15
+n_valid = 15
+n_test = 100
+print (n, n_valid, n_test)
+
+for i in np.arange(n_classes):
+    idx= data[labels==i,0]
+    np.random.shuffle(idx)
+                
+    if i==0:
+        indices_train = idx[:n]
+        indices_valid = idx[n:n+n_valid]
+        indices_test = idx[n+n_valid:n+n_valid+n_test]
+    else:
+        indices_train = np.hstack((indices_train,idx[:n]))
+        indices_valid = np.hstack((indices_valid,idx[n:n+n_valid]))
+        indices_test = np.hstack((indices_test,idx[n+n_valid:n+n_valid+n_test]))
+                                                                                    
+                                                                                                                    
+np.random.shuffle(indices_train)
+np.random.shuffle(indices_valid)
+np.random.shuffle(indices_test)
+np.savetxt('../data/cora/train_temp.txt', indices_train, fmt='%d')
+np.savetxt('../data/cora/dev_temp.txt', indices_valid, fmt='%d')   
+np.savetxt('../data/cora/test_temp.txt', indices_test, fmt='%d')
+
+
+### do hyperparameter search #####
+
+for i in [0.1, 1.0, 2.0]:
+    for j in [1.0, 10.0, 20.0]:
+        
+        args.mixup_alpha = i
+        args.mixup_consistency = j
+        print('mixup_alpha_'+str(args.mixup_alpha))
+        print('mixup_consistency_'+ str(args.mixup_consistency))
+
+
+        acc_list =[]
+        for i in np.arange(5):
+            acc_list.append(run(seed=i))
+            
+        acc = np.asarray(acc_list)
+        acc_mean = acc.mean()
+        acc_std = acc.std()
+        print ("mix_alpha, consis_coeff_"+ str(args.mixup_alpha)+'_'+ str(args.mixup_consistency)+':'+str(acc_mean)+'_'+str(acc_std))
 
 #if opt['save'] != '/':
 #    trainer_q.save(opt['save'] + '/gnnq.pt')
