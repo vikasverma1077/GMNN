93ff2cab7bef6f1d4b690eef40b2dd7419e9f8eb
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-16 11:20:48 +0000

    fcn predicted

--- semisupervised/codes/train.py
+++ semisupervised/codes/train.py
@@ -281,11 +281,13 @@ def pre_train(epoches):
         #loss = trainer_q.update_soft(inputs_q, target_q, idx_train)
         #import pdb; pdb.set_trace()
         ### create mix of feature and labels
-        rand_index = random.randint(0,1)
+        #rand_index = random.randint(0,1)
         #rand_index = 1
+        rand_index = 0
         if random.uniform(0,1) < 0.01:
             #print("Using gcn")
-            print("Using graphmix")
+            #print("Using graphmix")
+            print("Using FCN Predicted Targets")
         if rand_index == 0: ## do the augmented node training
             
             ## get the psudolabels for the unlabeled nodes ##
@@ -296,9 +298,10 @@ def pre_train(epoches):
             k = 10
             temp  = torch.zeros([k, target_q.shape[0], target_q.shape[1]], dtype=target_q.dtype)
             temp = temp.cuda()
+            tsum = 0.0
             for i in range(k):
-                temp[i,:,:] = trainer_q.predict_noisy(inputs_q)
-            target_predict = temp.mean(dim = 0)# trainer_q.predict(inputs_q)
+                tsum += trainer_q.predict_noisy(inputs_q * 1.0)
+            target_predict = tsum / k# trainer_q.predict(inputs_q)
             
             #target_predict = trainer_q.predict(inputs_q)
             target_predict = sharpen(target_predict,0.1)
@@ -310,6 +313,12 @@ def pre_train(epoches):
             #loss = trainer_q.update_soft_mix(inputs_q, target_q, idx_train)## for mixing features
             temp = torch.randint(0, idx_unlabeled.shape[0], size=(idx_train.shape[0],))## index of the samples chosen from idx_unlabeled
             idx_unlabeled_subset = idx_unlabeled[temp]
+            
+            #FOR FCN-predict
+            #for i in range(k):
+            #    temp[i,:,:] = trainer_q.predict_noisy_aux(inputs_q)
+            #idx_unlabeled_subset = idx_unlabeled[temp]
+            
             loss , loss_usup= trainer_q.update_soft_aux(inputs_q, target_q, target, idx_train, idx_unlabeled_subset, adj,  opt, mixup_layer =[1])## for augmented nodes
             mixup_consistency = get_current_consistency_weight(opt['mixup_consistency'], epoch)
             total_loss = loss + mixup_consistency*loss_usup
@@ -323,13 +332,14 @@ def pre_train(epoches):
             trainer_q.model.train()
             loss = trainer_q.update_soft(inputs_q, target_q, idx_train)
             
-            """
+            
             k = 10
             temp  = torch.zeros([k, target_q.shape[0], target_q.shape[1]], dtype=target_q.dtype)
             temp = temp.cuda()
+            tsum = 0.0
             for i in range(k):
-                temp[i,:,:] = trainer_q.predict_noisy(inputs_q)
-            target_predict = temp.mean(dim = 0)# trainer_q.predict(inputs_q)
+                tsum += trainer_q.predict_noisy(inputs_q * 1.0)
+            target_predict = tsum / k# trainer_q.predict(inputs_q)
             target_predict = sharpen(target_predict,0.1)
             target_q[idx_unlabeled] = target_predict[idx_unlabeled]
             
@@ -340,9 +350,9 @@ def pre_train(epoches):
 
             mixup_consistency = get_current_consistency_weight(opt['mixup_consistency'], epoch)
             total_loss = loss + mixup_consistency*loss_usup
-            """
             
-            total_loss = loss
+            
+            #total_loss = loss
             trainer_q.model.train()
             trainer_q.optimizer.zero_grad()
             total_loss.backward()
